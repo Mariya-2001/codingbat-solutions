6a1764c79ae063cec7ca9ed58c0010b292aac6c5
Mariya <lamki01@hotmail.com>
2025-08-03 00:20:38 +0400

    Add solution for Warm-up-posNeg

---
+++ warmup1/posNeg.java
@@ -0,0 +1,18 @@
+package codingbat.solutions.warmup1;
+
+public class posNeg {
+    public static void main(String[] args) {
+        System.out.println(posNeg(1, -1, false));
+        System.out.println(posNeg(-1, 1, false));
+        System.out.println(posNeg(-4, -5, true));
+    }
+
+    // Function done by Mariya
+    public static boolean posNeg(int a, int b, boolean negative) {
+        if (negative) {
+            return a < 0 && b < 0;
+        } else {
+            return (a < 0 && b > 0) || (a > 0 && b < 0);
+        }
+    }
+}
