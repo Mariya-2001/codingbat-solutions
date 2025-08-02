ec9673cdbe8d039cc4e16198866f6c0d63655fd4
Mariya <lamki01@hotmail.com>
2025-08-03 00:25:35 +0400

    Add solution for Warm-up-icyHot

---
+++ warmup1/icyHot.java
@@ -0,0 +1,14 @@
+package codingbat.solutions.warmup1;
+
+public class icyHot {
+    public static void main(String[] args) {
+        System.out.println(icyHot(120, -1));
+        System.out.println(icyHot(-1, 120));
+        System.out.println(icyHot(2, 120));
+    }
+
+    // Function done by Mariya
+    public static boolean icyHot(int temp1, int temp2) {
+        return (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0);
+    }
+}
